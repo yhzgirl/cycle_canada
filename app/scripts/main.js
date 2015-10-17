5f5018280776859eba6f9723b6c35bb71326d7d0
Michelle Brideau <pair+yhzgirl@gmail.com>
2015-10-17 10:19:22 +0100

    removing js from index.html to main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,3 +6,38 @@
 	});
 
 })(jQuery, window, document);
+
+/* horizontal scroll on mousewheel */
+$(document).ready(function () {
+	$('html, body, *').mousewheel(function (e, delta) {
+		this.scrollLeft -= (delta * 2);
+		e.preventDefault();
+	});
+});
+
+/* Instagram latest image */
+function instagram() {
+	jQuery.ajax({
+		url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=1910658902.aa2c401.cf848414fa6f43cd831db1c0895c79a9&callback=callback",
+		jsonp: "callback",
+		dataType: "jsonp",
+		success: function (response) {
+			var latestMedia = response.data[0].images.low_resolution.url;
+			var image = "<img src='" + latestMedia + "' class='img-responsive'>";
+			jQuery('.instagramMedia').html(image);
+		}
+	});
+}
+instagram();
+
+/* Twitter feed */
+! function (d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0],
+		p = /^http:/.test(d.location) ? 'http' : 'https';
+	if (!d.getElementById(id)) {
+		js = d.createElement(s);
+		js.id = id;
+		js.src = p + "://platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}
+}(document, "script", "twitter-wjs");
